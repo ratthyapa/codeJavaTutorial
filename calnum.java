f41e0a392e31f2b7824b1e6290f54d008fadb24a
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-07-26 13:35:41 +0700

    Update calnum.java

--- calnum.java
+++ calnum.java
@@ -2,7 +2,7 @@ import java.util.Scanner; //step1 เรียกใช้คลาส Scanner
 public class calnum {
     public static void main(String[] args) {
         int x, y;
-        Scanner in = new Scanner(System.in); //step2 สร้างออบเจ็กส์ in สำหรับรบข้อมูล
+        Scanner in = new Scanner(System.in); //step2 สร้างออบเจ็กส์ in สำหรับรับข้อมูล
         System.out.print("Input Number1 : ");
         x=in.nextInt(); //step3 รับตัวเลขเป็นจำนวนเต็มเก็บในตัวแปร x
         System.out.print("your number1 is" +x);
