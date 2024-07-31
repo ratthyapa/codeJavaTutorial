124c252392d6c0478a8efae499fce5d3806c79a4
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-07-31 15:46:23 +0700

    Create LoopNum.java

---
+++ LoopNum.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class LoopNum {
+    public static void main(String[] args) {
+        for(int number =1; number < 5; number++){
+            System.out.println(number+"");
+        }
+    }
+}
