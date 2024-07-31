bf55e85dcfbfa473f8e945057850493c1b90464e
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-07-31 15:46:28 +0700

    Create Squares.java

---
+++ Squares.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Squares {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 10; i++){
+            System.out.println(i+ " \t \t " +i*i);
+        }
+    }
+}
