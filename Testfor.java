a4201b5a8fc938710738fce85d1015b5b4967fbe
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-07-31 15:46:34 +0700

    Create Testfor.java

---
+++ Testfor.java
@@ -0,0 +1,7 @@
+public class Testfor {
+    public static void main(String[] args) {
+         for (int i = 1; i <= 10; i++) {
+            System.out.println("2 x " + i + " = " + (2 * i));
+    }
+}
+}
