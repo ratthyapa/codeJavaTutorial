6cf96a61db0556f980d1a4eff22a1a67797624fa
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-08-01 10:06:56 +0700

    Create whileloop.java

---
+++ whileloop.java
@@ -0,0 +1,12 @@
+public class whileloop {
+    public static void main(String[] args) {
+        for(int i =1;i<=10;i++){
+            System.out.println(+ i);
+        }
+        int iWhile = 1;
+        while (iWhile <=10) {
+            System.out.println("resui in whileloop : "+iWhile);
+            iWhile +=2 ;
+        }
+    }
+}
