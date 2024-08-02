a2eea06e51bed49339078dc66c4da4032d6d1e5d
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-08-02 15:01:38 +0700

    Create LocalVars.java

---
+++ LocalVars.java
@@ -0,0 +1,14 @@
+public class LocalVars {
+    public static void main(String[] args) {
+        phuket();
+        rayong();
+    }
+    public static void phuket(){
+        int birds = 500;
+        System.out.println("In phuket there are" +birds+"bidds");
+    }
+    public static void rayong(){
+        int birds = 3500;
+        System.out.println("In rayong there are" + birds+"bidds");
+    }
+}
