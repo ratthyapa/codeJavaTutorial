f8ecd68f382394cfe763197b6237ad54298e4716
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-08-02 11:12:19 +0700

    Create callname.java

---
+++ callname.java
@@ -0,0 +1,11 @@
+public class callname {
+    public static void main(String[] args) {
+        System.out.println("MY NAME IS ");
+        for (int i = 1; i <= 10; i++){
+            displaymessage();
+        }
+    }
+public static void displaymessage(){
+    System.out.println("ratthyapa utta");
+}
+}
