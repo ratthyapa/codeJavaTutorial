755dcafac12f1aee9e999824134e70ca0638d653
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-08-05 15:25:11 +0700

    Create Arraynitia.java

---
+++ Arraynitia.java
@@ -0,0 +1,9 @@
+public class Arraynitia {
+    public static void main(String[] args) {
+        int[] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        
+        for (int index = 0; index < days.length; index++) {
+            System.out.println("Month " + (index + 1) + " has " + days[index] + " days.");
+        }
+    }
+}
