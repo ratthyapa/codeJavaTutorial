fe571594f9c02048bdb56681a2f1ceefdb1717fc
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-08-05 15:24:49 +0700

    Create MonthDay.java

---
+++ MonthDay.java
@@ -0,0 +1,10 @@
+public class MonthDay {
+    public static void main(String[] args) {
+        String [ ] months = { "January", "February","March","April","May","June","July","Septmber","October","November","December"};
+        int [ ] days = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        for(int index = 0; index < months.length;index++){
+            System.out.println(months[index]+"has"+days[index]+"days.");
+        }
+    }
+}
+ 
