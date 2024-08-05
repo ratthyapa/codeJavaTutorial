97702e3aa52a2b788fa6a4f71d20997ffbe31de5
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-08-05 15:24:20 +0700

    Create TwoDArray.java

---
+++ TwoDArray.java
@@ -0,0 +1,21 @@
+public class TwoDArray {
+    public static void main(String[] args) {
+        int[][] table = new int[5][10];
+        
+        // Initialize the 2D array
+        for(int row = 0; row < table.length; row++) {
+            for(int col = 0; col < table[row].length; col++) {
+                table[row][col] = row * 10 + col;
+            }
+        }
+
+        // Print the 2D array
+        for(int row = 0; row < table.length; row++) {
+            for(int col = 0; col < table[row].length; col++) {
+                System.out.print(table[row][col] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
+
