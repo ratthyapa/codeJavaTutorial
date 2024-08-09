d2acf901e67811f37ac5b4ca4ef71672487c86e9
ratthyapa <152243481+ratthyapa@users.noreply.github.com>
2024-08-09 12:43:17 +0700

    Create calOverload.java

---
+++ calOverload.java
@@ -0,0 +1,14 @@
+public class calOverload {
+   public static void main(String[] args) {
+    OverloadDemo o1 = new OverloadDemo();
+    OverloadDemo o2 = new OverloadDemo(88);
+    OverloadDemo o3 = new OverloadDemo(24.7);
+    OverloadDemo o4 = new OverloadDemo(2.4);
+    System.out.println("O1.x :" +o1.x);
+    System.out.println("O2.x :" +o2.x);
+    System.out.println("O3.x :" +o3.x);
+    System.out.println("O4.x :" +o4.x);
+    
+
+   }
+}
